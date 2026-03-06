073e620f5d12e52f69d3e520ce0193e9501aad3d
jala-asadli <jasadli16143@ada.edu.az>
2026-03-06 10:37:45 +0400

    EX03

---
+++ EX03/Main.java
@@ -0,0 +1,39 @@
+class Rectangle {
+    int width, height;
+
+    public Rectangle(int w, int h) {
+        width = w;
+        height = h;
+    }
+
+    public boolean equals(Object obj) {
+        Rectangle rect = (Rectangle) obj;
+
+        if (this.width == rect.width && this.height == rect.height) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class Square extends Rectangle {
+
+    public Square(int side) {
+        super(side, side);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Object o1 = new Rectangle(5, 10);
+        Object o2 = new Rectangle(15, 15);
+        Object o3 = new Square(15);
+
+        System.out.println("Objects are identical: " + o1.equals(o2));
+        System.out.println("Objects are identical: " + o1.equals(o3));
+        System.out.println("Objects are identical: " + o2.equals(o3));
+
+    }
+}
